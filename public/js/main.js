33c92ac2ffe49822b5acffb5598583f55d3de118
Abi Munoz <abimunozr@gmail.com>
2017-02-27 12:48:07 -0800

    attempts to add attribute to aframe entity on frontend

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
-//
+$(document).ready(function(){
+  $('#submit').click(function(e){
+    console.log('click');
+    const input = $('#input').val();
+    console.log(input);
+    const aframeText = $('#aframeText');
+    aframeText.append(input);
+  })
+})
