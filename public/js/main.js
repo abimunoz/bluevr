a131faad97a7f5f83eda989482d35862fb4c4802
Abi Munoz <abimunozr@gmail.com>
2017-02-27 17:14:25 -0800

    appends input text with component to page

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,21 @@
+// APPENDS COMPONENT WITH INPUT VALUE
+$(document).ready(function(){
+  $('#submit').click(function(e){
+    const scene = $('#scene');
+    const input = $('#input').val();
+    const component = `<a-entity
+        id="aframeText"
+        geometry="primitive: plane; width: 4; height: 4"
+        material="color: red"
+        position="0 2 -5"
+        text="value: ${input}"></a-entity>`
+        scene.append(component)
+  })
+});
+
+
+
+
 // CHANGES VALUE OF INNER TEXT TO INPUT VALUE
 // $(document).ready(function(){
 //   $('#submit').click(function(e){
@@ -8,15 +26,15 @@
 // });
 
 // APPENDS AFRAME TEXT COMPONENT TO SCENE
-$(document).ready(function(){
-  $('#submit').click(function(e){
-    const scene = $('#scene');
-    const component = `<a-entity
-    id="aframeText"
-    geometry="primitive: plane; width: 4; height: 4"
-    material="color: red"
-    position="0 2 -5"
-    text="value: This text will be 4 units wide."></a-entity>`
-    scene.append(component)
-  })
-});
+// $(document).ready(function(){
+//   $('#submit').click(function(e){
+//     const scene = $('#scene');
+//     const component = `<a-entity
+//     id="aframeText"
+//     geometry="primitive: plane; width: 4; height: 4"
+//     material="color: red"
+//     position="0 2 -5"
+//     text="value: This text will be 4 units wide."></a-entity>`
+//     scene.append(component)
+//   })
+// });
