5ed43d004dee912f3b474b658a61245563b1a4e7
Abi Munoz <abimunozr@gmail.com>
2017-02-27 17:05:57 -0800

    appends component to scene

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,22 @@
-// d = document.querySelector('#aframeText');
-// console.log(d);
-// d.setAttribute('text', "value: 'hi'");
-// console.log(d);
+// CHANGES VALUE OF INNER TEXT TO INPUT VALUE
+// $(document).ready(function(){
+//   $('#submit').click(function(e){
+//     const aframeText = $('#aframeText');
+//     const input = $('#input').val();
+//     aframeText.attr('text', `value:${input}`)
+//   })
+// });
 
+// APPENDS AFRAME TEXT COMPONENT TO SCENE
 $(document).ready(function(){
   $('#submit').click(function(e){
-    const aframeText = $('#aframeText');
-    const input = $('#input').val();
-    aframeText.attr('text', `value:${input}`)
+    const scene = $('#scene');
+    const component = `<a-entity
+    id="aframeText"
+    geometry="primitive: plane; width: 4; height: 4"
+    material="color: red"
+    position="0 2 -5"
+    text="value: This text will be 4 units wide."></a-entity>`
+    scene.append(component)
   })
 });
