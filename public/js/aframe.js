24ca93230e4a7bcdb7969b9382c542d5eb11d795
Abi Munoz <abimunozr@gmail.com>
2017-03-01 16:42:28 -0800

    console logs mongo id

--- public/js/aframe.js
+++ public/js/aframe.js
@@ -6,6 +6,7 @@ AFRAME.registerComponent('cursor-listener', {
       var randomIndex = Math.floor(Math.random() * COLORS.length);
       this.setAttribute('material', 'color', COLORS[randomIndex]);
       console.log('I was clicked at: ', evt.detail.intersection.point);
+      console.log(this);
     });
   }
 });
